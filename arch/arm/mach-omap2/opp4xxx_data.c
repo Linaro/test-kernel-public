7731921be108af2b5fbb44531c355636834151bb
Misael Lopez Cruz <misael.lopez@ti.com>
2011-07-08 21:28:51 -0500

    OMAP4: OPP: Add OPPs for AESS
    Add OPPs for Audio Engine Subsystem.
    
    Acked-by: Nishanth Menon <nm@ti.com>
    Signed-off-by: Misael Lopez Cruz <misael.lopez@ti.com>

--- arch/arm/mach-omap2/opp4xxx_data.c
+++ arch/arm/mach-omap2/opp4xxx_data.c
@@ -167,7 +167,11 @@ static struct omap_opp_def __initdata omap443x_opp_def_list[] = {
 	OPP_INITIALIZER("iva", "dpll_iva_m5x2_ck", "iva", true, 266100000, OMAP4430_VDD_IVA_OPP100_UV),
 	/* IVA OPP3 - OPP-Turbo */
 	OPP_INITIALIZER("iva", "dpll_iva_m5x2_ck", "iva", false, 332000000, OMAP4430_VDD_IVA_OPPTURBO_UV),
-	/* TODO: add DSP, aess, fdif, gpu */
+        /* ABE OPP1 - OPP50 */                                                  
+        OPP_INITIALIZER("aess", "dpll_iva_m5x2_ck", "aess", true, 98304000, OMAP4430_VDD_IVA_OPP50_UV),     
+        /* ABE OPP2 - OPP100 */                                                 
+        OPP_INITIALIZER("aess", "dpll_iva_m5x2_ck", "aess", true, 196608000, OMAP4430_VDD_IVA_OPP100_UV),   
+        /* TODO: add DSP, fdif, gpu */ 
 };
 
 #define OMAP4460_VDD_MPU_OPP50_UV		1025000
@@ -286,6 +290,7 @@ static struct omap_opp_def __initdata omap446x_opp_def_list[] = {
 	/* IVA OPP5 - OPP-Nitro SpeedBin*/
 	OPP_INITIALIZER("iva", "dpll_iva_m5x2_ck", "iva", false, 500000000, OMAP4460_VDD_IVA_OPPNITRO_UV),
 
+<<<<<<< current
 #if 0
 	/* SGX OPP1 - OPP50 */
 	OPP_INITIALIZER("gpu", "dpll_per_m7x2_ck", "core", true, 153600000, OMAP4460_VDD_CORE_OPP50_UV),
@@ -314,6 +319,11 @@ static struct omap_opp_def __initdata omap446x_opp_def_list[] = {
 	/* ABE OPP2 - OPP100 */
 	OPP_INITIALIZER("aess", "abe_clk", "iva", true, 196608000, OMAP4460_VDD_IVA_OPP100_UV),
 #endif
+	/* ABE OPP1 - OPP50 */                                                  
+        OPP_INITIALIZER("aess", "dpll_iva_m5x2_ck", "aess", true, 98304000, OMAP4460_VDD_IVA_OPP50_UV),
+        /* ABE OPP2 - OPP100 */                                                 
+        OPP_INITIALIZER("aess", "dpll_iva_m5x2_ck", "aess", true, 196608000, OMAP4460_VDD_IVA_OPP100_UV),
+
 };
 
 /**
