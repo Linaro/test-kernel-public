ae26cda5cbffd7c830ded367b6fa978eb70c2405
Sachin Kamat <sachin.kamat@linaro.org>
2011-07-19 11:58:05 +0530

    Input: s3c_ts - Added touch support for s3c_ts
    Added touch screen support for SMDKV310. This touch screen
    is mounted on 3.7" LCD panel and is connected to LCD A port.
    
    Signed-off-by: Sushant Kondguli <sushant.sk@samsung.com>
    Signed-off-by: Tushar Behera <tushar.behera@linaro.org>
    Signed-off-by: Fahad Kunnathadi <fahad.k@samsung.com>
    
    Conflicts:
    
    	arch/arm/mach-exynos4/mach-smdkv310.c
    
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>

--- arch/arm/plat-samsung/include/plat/ts.h
+++ arch/arm/plat-samsung/include/plat/ts.h
@@ -22,4 +22,35 @@ extern void s3c24xx_ts_set_platdata(struct s3c2410_ts_mach_info *);
 /* defined by architecture to configure gpio */
 extern void s3c24xx_ts_cfg_gpio(struct platform_device *dev);
 
+enum s3c_adc_type {
+	ADC_TYPE_0,
+	ADC_TYPE_1,     /*    S3C2416, S3C2450 */
+	ADC_TYPE_2,     /*    S3C64XX, S5PC1XX */
+};
+
+struct s3c_ts_mach_info {
+	int                     delay;
+	int                     presc;
+	int                     oversampling_shift;
+	int                     resol_bit;
+	enum s3c_adc_type       s3c_adc_con;
+};
+
+struct s3c_ts_info {
+	struct input_dev        *dev;
+	long                    xp;
+	long                    yp;
+	int                     count;
+	int                     shift;
+	char                    phys[32];
+	int                     resol_bit;
+	enum s3c_adc_type       s3c_adc_con;
+
+#ifdef CONFIG_HAS_WAKELOCK
+	struct early_suspend early_suspend;
+#endif
+};
+
+extern void __init s3c_ts_set_platdata(struct s3c_ts_mach_info *pd);
+
 #endif /* __ASM_ARM_TS_H */
