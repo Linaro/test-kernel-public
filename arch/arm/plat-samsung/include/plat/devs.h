bfa4dd424b43ba51839d05ff639b74e6eed080b9
Bhuvana Kakunoori <bhuvana.kakunoori@linaro.org>
2011-08-05 11:07:52 +0530

    ARM: S5P: Add USB OHCI platform device definition
    This patch adds support for OHCI device definition on
    Samsung S5P series platforms
    
    Signed-off-by: Bhuvana Kakunoori <bhuvana.kakunoori@linaro.org>

--- arch/arm/plat-samsung/include/plat/devs.h
+++ arch/arm/plat-samsung/include/plat/devs.h
@@ -152,6 +152,7 @@ extern struct platform_device s5p_device_mipi_csis0;
 extern struct platform_device s5p_device_mipi_csis1;
 
 extern struct platform_device s5p_device_ehci;
+extern struct platform_device s5p_device_ohci;
 
 extern struct platform_device exynos4_device_sysmmu;
 
