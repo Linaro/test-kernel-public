d6d3affae8cbc90416ae8baf1b8f1a32b1da5e02
Pankaj <pankaj.dubey@samsung.com>
2011-08-11 13:57:44 +0530

    ARM: EXYNOS4: Add support of UDC for origen board
    Added USB device controller support on origen machine file.
    
    Signed-off-by: Pankaj Dubey <pankaj.dubey@samsung.com>
    Signed-off-by: Sachin Kamat <sachin.kamat@linaro.org>

--- arch/arm/mach-exynos/mach-origen.c
+++ arch/arm/mach-exynos/mach-origen.c
@@ -42,6 +42,7 @@
 #include <plat/pd.h>
 #include <plat/fb.h>
 #include <plat/mfc.h>
+#include <plat/otg.h>
 
 #include <mach/map.h>
 
@@ -497,6 +498,16 @@ static void __init origen_ohci_init(void)
 	s5p_ohci_set_platdata(pdata);
 }
 
+/* USB OTG */
+static struct s5p_otg_platdata origen_otg_pdata;
+
+static void __init origen_otg_init(void)
+{
+	struct s5p_otg_platdata *pdata = &origen_otg_pdata;
+
+	s5p_otg_set_platdata(pdata);
+}
+
 static struct gpio_keys_button origen_gpio_keys_table[] = {
 	{
 		.code			= KEY_MENU,
@@ -625,6 +636,7 @@ static struct platform_device *origen_devices[] __initdata = {
 	&s3c_device_hsmmc0,
 	&s3c_device_i2c0,
 	&s3c_device_rtc,
+	&s3c_device_usbgadget,
 	&s3c_device_wdt,
 	&s5p_device_ehci,
 	&s5p_device_fimc0,
@@ -715,6 +727,7 @@ static void __init origen_machine_init(void)
 
 	origen_ehci_init();
 	origen_ohci_init();
+	origen_otg_init();
 	clk_xusbxti.rate = 24000000;
 
 	s5p_tv_setup();
