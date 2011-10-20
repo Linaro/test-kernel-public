358d48bdcb8c9877255b3705fa5f3f7182819112
Ohad Ben-Cohen <ohad@wizery.com>
2011-10-20 20:25:53 +0200

    ARM: OMAP: add remoteproc devices for OMAP4
    Add an omap-rproc device with which one can start using OMAP4's
    dual-M3 "Ducati" ipu subsystem.
    
    Currently we're adding support only for the first M3 core (hence
    the name "ipu_c0"); support for the second M3 core, as well as for
    the DSP subsystem, will be added later on.
    
    Bind this device to its dedicated IOMMU device, and assign it a
    dedicated mailbox instance too.
    
    In addition, reserve it a physically contiguous memory region using
    the upcoming CMA mechanism (this makes this patch impossible to merge
    at this point).
    
    Note: at this point we're using a fixed CMA base address (as defined by
    OMAP_RPROC_CMA_BASE), but this will go away once the generic iommu-based
    DMA API will materialize, because at that point we will (almost) not care about
    the physical location of the CMA memory.
    
    Based on (but now quite far from) work done by Fernando Guzman Lugo
    <fernando.lugo@ti.com>.
    
    Designed with Brian Swetland <swetland@google.com>.
    
    Signed-off-by: Ohad Ben-Cohen <ohad@wizery.com>
    Acked-by: Tony Lindgren <tony@atomide.com>
    Cc: Brian Swetland <swetland@google.com>
    Cc: Arnd Bergmann <arnd@arndb.de>
    Cc: Grant Likely <grant.likely@secretlab.ca>
    Cc: Russell King <linux@arm.linux.org.uk>
    Cc: Rusty Russell <rusty@rustcorp.com.au>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Greg KH <greg@kroah.com>
    Cc: Stephen Boyd <sboyd@codeaurora.org>
    
    Conflicts:
    
    	arch/arm/plat-omap/common.c
    	drivers/remoteproc/Kconfig

--- arch/arm/plat-omap/common.c
+++ arch/arm/plat-omap/common.c
@@ -21,7 +21,9 @@
 #include <plat/vram.h>
 #include <plat/dsp.h>
 
+#include <plat/drm.h>
 #include <plat/omap-secure.h>
+#include <plat/remoteproc.h>
 
 
 #define NO_LENGTH_CHECK 0xffffffff
@@ -68,6 +70,7 @@ void __init omap_reserve(void)
 	omap_dsp_reserve_sdram_memblock();
 	omap_secure_ram_reserve_memblock();
 	omap_barrier_reserve_memblock();
+	omap_rproc_reserve_cma();
 }
 
 void __init omap_init_consistent_dma_size(void)
