10ff4bfa9e44055e17f00ba55b5e45a0912afe4c
Inderpal Singh <inderpal.singh@linaro.org>
2011-11-22 09:55:36 +0530

    ARM: exynos4: Add support for AFTR mode cpuidle state
    This patch adds support for AFTR(ARM OFF TOP RUNNING) mode in
    cpuidle driver for EXYNOS4210. L2 cache keeps their data in this mode.
    This patch ports the code to the latest interfaces to
    save/restore CPU state inclusive of CPU PM notifiers, l2
    resume and cpu_suspend/resume.
    
    Signed-off-by: Jaecheol Lee <jc.lee@samsung.com>
    Signed-off-by: Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
    Signed-off-by: Amit Daniel Kachhap <amit.kachhap@linaro.org>
    Signed-off-by: Inderpal Singh <inderpal.singh@linaro.org>

--- arch/arm/mach-exynos/include/mach/pmu.h
+++ arch/arm/mach-exynos/include/mach/pmu.h
@@ -27,6 +27,8 @@ struct exynos4_pmu_conf {
 	unsigned int val[NUM_SYS_POWERDOWN];
 };
 
+extern unsigned long l2x0_regs_phys;
 extern void exynos4_sys_powerdown_conf(enum sys_powerdown mode);
+extern void s3c_cpu_resume(void);
 
 #endif /* __ASM_ARCH_PMU_H */
