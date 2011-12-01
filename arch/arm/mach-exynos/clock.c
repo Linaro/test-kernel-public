63552451c5991580c00daca70806634300be3465
Tushar Behera <tushar.behera@linaro.org>
2011-12-01 09:30:11 +0530

    ARM: EXYNOS: Add clkdev lookup entry for lcd clock
    The framebuffer driver needs the clock named 'lcd' as its bus
    clock but the equivalent clock on Exynos4 is named as 'fimd'.
    Hence, create a clkdev lookup entry with the name 'lcd' that
    references the 'fimd' clock.
    
    Signed-off-by: Tushar Behera <tushar.behera@linaro.org>

--- arch/arm/mach-exynos/clock.c
+++ arch/arm/mach-exynos/clock.c
@@ -508,11 +508,6 @@ static struct clk init_clocks_off[] = {
 		.enable		= exynos4_clk_ip_cam_ctrl,
 		.ctrlbit	= (1 << 3),
 	}, {
-		.name		= "fimd",
-		.devname	= "exynos4-fb.0",
-		.enable		= exynos4_clk_ip_lcd0_ctrl,
-		.ctrlbit	= (1 << 0),
-	}, {
 		.name		= "hsmmc",
 		.devname	= "s3c-sdhci.0",
 		.parent		= &clk_aclk_133.clk,
@@ -810,6 +805,13 @@ static struct clk clk_mdma1 = {
 	.devname	= "dma-pl330.2",
 	.enable		= exynos4_clk_ip_image_ctrl,
 	.ctrlbit	= ((1 << 8) | (1 << 5) | (1 << 2)),
+};
+
+static struct clk clk_fimd0 = {
+	.name		= "fimd",
+	.devname	= "exynos4-fb.0",
+	.enable		= exynos4_clk_ip_lcd0_ctrl,
+	.ctrlbit	= (1 << 0),
 };
 
 struct clk *clkset_group_list[] = {
@@ -1345,6 +1347,7 @@ static struct clk *clk_cdev[] = {
 	&clk_pdma0,
 	&clk_pdma1,
 	&clk_mdma1,
+	&clk_fimd0,
 };
 
 static struct clksrc_clk *clksrc_cdev[] = {
@@ -1374,6 +1377,7 @@ static struct clk_lookup exynos4_clk_lookup[] = {
 	CLKDEV_INIT("dma-pl330.0", "apb_pclk", &clk_pdma0),
 	CLKDEV_INIT("dma-pl330.1", "apb_pclk", &clk_pdma1),
 	CLKDEV_INIT("dma-pl330.2", "apb_pclk", &clk_mdma1),
+	CLKDEV_INIT("exynos4-fb.0", "lcd", &clk_fimd0),
 };
 
 static int xtal_rate;
