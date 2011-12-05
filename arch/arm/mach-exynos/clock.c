d4a73edf383f6a1adf236e0fe494a03938118476
Tushar Behera <tushar.behera@linaro.org>
2011-12-05 14:42:29 +0530

    ARM: EXYNOS: Add apb_pclk clkdev entry for mdma1
    Amba core assumes the pclk to be named as apb_pclk. During device probe,
    it tries to get that clock and enable that. When PM_RUNTIME is enabled,
    dma clock is not explicitly enabled in pl330_probe, which causes device
    probe to fail. Adding a clkdev entry for apb_pclk for mdma1 fixes the
    problem.
    
    This patch fixes following runtime error.
    
    dma-pl330 dma-pl330.2: PERIPH_ID 0x0, PCELL_ID 0x0 !
    dma-pl330: probe of dma-pl330.2 failed with error -22
    
    Signed-off-by: Tushar Behera <tushar.behera@linaro.org>

--- arch/arm/mach-exynos/clock.c
+++ arch/arm/mach-exynos/clock.c
@@ -1336,9 +1336,7 @@ static struct clk_lookup exynos4_clk_lookup[] = {
 	CLKDEV_INIT("s3c-sdhci.3", "mmc_busclk.2", &clk_sclk_mmc3.clk),
 	CLKDEV_INIT("dma-pl330.0", "apb_pclk", &clk_pdma0),
 	CLKDEV_INIT("dma-pl330.1", "apb_pclk", &clk_pdma1),
-	CLKDEV_INIT("s3c64xx-spi.0", "spi_busclk0", &clk_sclk_spi0.clk),
-	CLKDEV_INIT("s3c64xx-spi.1", "spi_busclk0", &clk_sclk_spi1.clk),
-	CLKDEV_INIT("s3c64xx-spi.2", "spi_busclk0", &clk_sclk_spi2.clk),
+	CLKDEV_INIT("dma-pl330.2", "apb_pclk", &clk_mdma1),
 };
 
 static int xtal_rate;
