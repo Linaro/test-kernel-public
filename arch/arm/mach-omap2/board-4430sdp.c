083e3862d0c4af0c17ec9cce5ae82a3654a19dde
Peter Ujfalusi <peter.ujfalusi@ti.com>
2012-01-26 14:36:01 +0200

    OMAP: 4430sdp: Correct fixed regulator device ID
    The board has two fixed voltage regulator. Correct the
    device ID for the vbat, which used -1.
    Create defines for the fixed regulator IDs so when adding new
    regulators we know the next available ID to use for them.
    
    Signed-off-by: Peter Ujfalusi <peter.ujfalusi@ti.com>

--- arch/arm/mach-omap2/board-4430sdp.c
+++ arch/arm/mach-omap2/board-4430sdp.c
@@ -72,6 +72,8 @@
 #define GPIO_WIFI_IRQ		53
 
 #define TPS62361_GPIO   7
+#define FIXED_REG_VBAT_ID	0
+#define FIXED_REG_VWLAN_ID	1
 
 static struct omap_board_config_kernel sdp4430_config[] __initdata = {
 };
@@ -382,7 +384,7 @@ static struct fixed_voltage_config sdp4430_vbat_pdata = {
 
 static struct platform_device sdp4430_vbat = {
 	.name		= "reg-fixed-voltage",
-	.id		= -1,
+	.id		= FIXED_REG_VBAT_ID,
 	.dev = {
 		.platform_data = &sdp4430_vbat_pdata,
 	},
@@ -497,7 +499,7 @@ static struct fixed_voltage_config sdp4430_vwlan = {
 
 static struct platform_device omap_vwlan_device = {
 	.name		= "reg-fixed-voltage",
-	.id		= 1,
+	.id		= FIXED_REG_VWLAN_ID,
 	.dev = {
 		.platform_data = &sdp4430_vwlan,
 	},
