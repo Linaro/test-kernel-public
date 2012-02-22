4e935b099d552ee676f14c1ad5a52c1a00b72c90
Vishwanath BS <vishwanath.bs@ti.com>
2012-02-22 23:00:41 +0800

    OMAP5 PM: Initialize Processor devices for OMAP5
    Patch initializes mpu_dev, iva_dev  and l3_dev for OMAP5 which are
    needed by OPP layer subsequently.
    
    Signed-off-by: Vishwanath BS <vishwanath.bs@ti.com>

--- arch/arm/mach-omap2/pm.c
+++ arch/arm/mach-omap2/pm.c
@@ -93,7 +93,7 @@ static void omap2_init_processor_devices(void)
 	if (omap3_has_iva())
 		_init_omap_device("iva");
 
-	if (cpu_is_omap44xx()) {
+	if (cpu_is_omap44xx() || cpu_is_omap54xx()) {
 		_init_omap_device("l3_main_1");
 		_init_omap_device("dsp");
 		_init_omap_device("iva");
@@ -425,6 +425,7 @@ static int __init omap2_pm_qos_tput_init(void)
 
 static int __init omap2_common_pm_init(void)
 {
+#ifndef CONFIG_MACH_OMAP_5430ZEBU
 	if (cpu_is_omap54xx()) {
 		pr_err("FIXME: omap2_common_pm_init\n");
 		return 0;
@@ -433,6 +434,7 @@ static int __init omap2_common_pm_init(void)
         if (!of_have_populated_dt())                                            
                 omap2_init_processor_devices(); 
 
+#endif
 	omap_pm_if_init();
 
 	/* Register to the per-device PM QoS framework */
