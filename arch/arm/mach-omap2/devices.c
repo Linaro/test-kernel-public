3de1b091d4508391007a8055e30ef77cb45e4fbc
Andy Green <andy.green@linaro.org>
2012-02-25 00:26:18 +0800

    uplevel omap5 asoc devices use new generic dmic codec config name
    Signed-off-by: Andy Green <andy.green@linaro.org>

--- arch/arm/mach-omap2/devices.c
+++ arch/arm/mach-omap2/devices.c
@@ -444,7 +444,7 @@ static void omap_init_mcpdm(void)
 static inline void omap_init_mcpdm(void) {}
 #endif
 
-#if defined(CONFIG_SND_OMAP_SOC_DMIC) || \
+#if defined(CONFIG_SND_OMAP_SOC_DMIC) || defined(CONFIG_SND_SOC_DMIC) || \
 		defined(CONFIG_SND_OMAP_SOC_DMIC_MODULE)
 
 static void omap_init_dmic(void)
