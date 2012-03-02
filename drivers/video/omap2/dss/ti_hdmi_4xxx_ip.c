67993252e65128b1638e6300689f0a2bed6cd631
Andy Green <andy.green@linaro.org>
2012-03-03 07:11:05 +0800

    dss hdmi phy free irq only if allocated
    Signed-off-by: Andy Green <andy.green@linaro.org>

--- drivers/video/omap2/dss/ti_hdmi_4xxx_ip.c
+++ drivers/video/omap2/dss/ti_hdmi_4xxx_ip.c
@@ -348,6 +348,12 @@ int ti_hdmi_4xxx_phy_poweron(struct hdmi_ip_data *ip_data)
 	if (cpu_is_omap54xx())
 		REG_FLD_MOD(phy_base, HDMI_TXPHY_BIST_CONTROL, 1, 11, 11);
 
+	ip_data->has_irq = true;
+
+	/* enable divby2 */
+	if (cpu_is_omap54xx())
+		REG_FLD_MOD(phy_base, HDMI_TXPHY_BIST_CONTROL, 1, 11, 11);
+
 	return 0;
 }
 
@@ -362,8 +368,10 @@ int ti_hdmi_4xxx_phy_enable(struct hdmi_ip_data *ip_data)
 
 void ti_hdmi_4xxx_phy_disable(struct hdmi_ip_data *ip_data)
 {
-	free_irq(gpio_to_irq(ip_data->hpd_gpio), ip_data);
-
+	if (ip_data->has_irq) {
+		free_irq(gpio_to_irq(ip_data->hpd_gpio), ip_data);
+		ip_data->has_irq = false;
+	}
 	hdmi_set_phy_pwr(ip_data, HDMI_PHYPWRCMD_OFF);
 	ip_data->phy_tx_enabled = false;
 	ip_data->cfg.hdmi_phy_tx_enabled = 0;
