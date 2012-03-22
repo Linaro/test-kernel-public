5302796eaba565ffca9c02d0aa224f2c5e395718
Miguel Vadillo <vadillo@ti.com>
2012-03-22 08:39:03 +0800

    omap5: opp: fix fdif clk name in opp tables
    OPP table has a wrong name for fdif clock, fixed the same.
    
    Signed-off-by: Miguel Vadillo <vadillo@ti.com>
    Acked-by: Vishwanath BS <vishwanath.bs@ti.com>

--- arch/arm/mach-omap2/opp5xxx_data.c
+++ arch/arm/mach-omap2/opp5xxx_data.c
@@ -173,11 +173,23 @@ static struct omap_opp_def __initdata omap54xx_opp_def_list[] = {
 	OPP_INITIALIZER("l3_main_1", "virt_l3_ck", "core", true, 266000000, OMAP5430_VDD_CORE_OPP_NOM),
 	/* TODO: Uncomment the following once we have drivers for them */
 #if 0
-	/* FDIF OPP1 - OPPLOW */
-	OPP_INITIALIZER("fdif", "fdif_fck", "core", true, 64000000, OMAP5430_VDD_CORE_OPP_LOW),
-	/* FDIF OPP3 - OPPNOM */
-	OPP_INITIALIZER("fdif", "fdif_fck", "core", true, 128000000, OMAP5430_VDD_CORE_OPP_NOM),
+	/* MMC1 OPP1 - OPPLOW */
+	OPP_INITIALIZER("mmc1", "mmc1_fclk", "core", true, 96000000, OMAP5430_VDD_CORE_OPP_LOW),
+	/* MMC1 OPP2 - OPPNOM */
+	OPP_INITIALIZER("mmc1", "mmc1_fclk", "core", true, 192000000, OMAP5430_VDD_CORE_OPP_NOM),
 
+	/* MMC2 OPP1 - OPPLOW */
+	OPP_INITIALIZER("mmc2", "mmc2_fclk", "core", true, 96000000, OMAP5430_VDD_CORE_OPP_LOW),
+	/* MMC2 OPP2 - OPPNOM */
+	OPP_INITIALIZER("mmc2", "mmc2_fclk", "core", true, 192000000, OMAP5430_VDD_CORE_OPP_NOM),
+#endif
+
+	/* FDIF OPP1 - OPPLOW */
+	OPP_INITIALIZER("fdif", "fdif_fclk", "core", true, 64000000, OMAP5430_VDD_CORE_OPP_LOW),
+	/* FDIF OPP2 - OPPNOM */
+	OPP_INITIALIZER("fdif", "fdif_fclk", "core", true, 128000000, OMAP5430_VDD_CORE_OPP_NOM),
+
+#if 0
 	/* HSI OPP1 - OPPLOW */
 	OPP_INITIALIZER("hsi", "hsi_fck", "core", true, 96000000, OMAP5430_VDD_CORE_OPP_LOW),
 	/* FDIF OPP3 - OPPNOM */
