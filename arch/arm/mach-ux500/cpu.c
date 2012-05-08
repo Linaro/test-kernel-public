027dd1e0121a70d21abb0214d874cba67cb22798
Arnd Bergmann <arnd@arndb.de>
2012-05-08 15:10:25 +0000

    ARM: ux500: fixes for multiplatform
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-ux500/cpu.c
+++ arch/arm/mach-ux500/cpu.c
@@ -70,12 +70,16 @@ void __init ux500_init_irq(void)
 	 */
 	if (cpu_is_u8500_family())
 		db8500_prcmu_early_init();
+#ifndef CONFIG_COMMON_CLK
 	clk_init();
+#endif
 }
 
 void __init ux500_init_late(void)
 {
+#ifndef CONFIG_COMMON_CLK
 	clk_debugfs_init();
+#endif
 	clk_init_smp_twd_cpufreq();
 }
 
