fe735cc819be22e897690a36a1c3fd0bdfc24ef8
Arnd Bergmann <arnd@arndb.de>
2012-05-27 01:50:46 +0000

    [ARM] warnings in arch/arm/mach-shmobile/include/mach/emev2.h
    Without this patch, building kzm9g_defconfig results in:
    
    arch/arm/mach-shmobile/built-in.o: In function `shmobile_platform_cpu_kill':
    pfc-sh73a0.c:(.text+0x644): undefined reference to `emev2_platform_cpu_kill'
    arch/arm/mach-shmobile/built-in.o: In function `smp_init_cpus':
    pfc-sh73a0.c:(.init.text+0x940): undefined reference to `emev2_get_core_count'
    arch/arm/mach-shmobile/built-in.o: In function `platform_smp_prepare_cpus':
    pfc-sh73a0.c:(.init.text+0x9d8): undefined reference to `emev2_smp_prepare_cpus'
    arch/arm/mach-shmobile/built-in.o: In function `platform_secondary_init':
    pfc-sh73a0.c:(.cpuinit.text+0x3c): undefined reference to `emev2_secondary_init'
    arch/arm/mach-shmobile/built-in.o: In function `boot_secondary':
    pfc-sh73a0.c:(.cpuinit.text+0x8c): undefined reference to `emev2_boot_secondary'
    make[1]: *** [.tmp_vmlinux1] Error 1
    make: *** [sub-make] Error 2
    
    Generated by script, please check manually
    
    Signed-off-by: Arnd Bergmann <arnd@arndb.de>

--- arch/arm/mach-shmobile/include/mach/emev2.h
+++ arch/arm/mach-shmobile/include/mach/emev2.h
@@ -1,6 +1,8 @@
 #ifndef __ASM_EMEV2_H__
 #define __ASM_EMEV2_H__
 
+#ifdef CONFIG_ARCH_EMEV2
+
 extern void emev2_map_io(void);
 extern void emev2_init_irq(void);
 extern void emev2_add_early_devices(void);
@@ -12,6 +14,13 @@ extern int emev2_platform_cpu_kill(unsigned int cpu);
 extern void emev2_secondary_init(unsigned int cpu);
 extern int emev2_boot_secondary(unsigned int cpu);
 extern void emev2_smp_prepare_cpus(void);
+#else
+static inline unsigned int emev2_get_core_count(void) { return 1; };
+static inline int emev2_platform_cpu_kill(unsigned int cpu) { return 0; }
+static inline void emev2_secondary_init(unsigned int cpu) {}
+static inline int emev2_boot_secondary(unsigned int cpu) { return 0; }
+static inline void emev2_smp_prepare_cpus(void) {}
+#endif
 
 #define EMEV2_GPIO_BASE 200
 #define EMEV2_GPIO_IRQ(n) (EMEV2_GPIO_BASE + (n))
