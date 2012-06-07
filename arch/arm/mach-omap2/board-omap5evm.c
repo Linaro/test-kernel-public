0a619beaf714553a817a0bca107b65d776d92164
Andy Green <andy.green@linaro.org>
2012-06-07 20:57:45 +0800

    clean remove all warnings from build
    Signed-off-by: Andy Green <andy.green@linaro.org>

--- arch/arm/mach-omap2/board-omap5evm.c
+++ arch/arm/mach-omap2/board-omap5evm.c
@@ -893,7 +893,7 @@ static struct i2c_board_info __initdata omap5evm_i2c_1_boardinfo[] = {
 
 };
 
-
+#if 0
 static struct qtm_touch_keyarray_cfg omap5evm_key_array_data[] = {
 	{
 		.ctrl = 0,
@@ -1101,6 +1101,7 @@ static struct i2c_board_info __initdata omap5evm_i2c_4_boardinfo[] = {
 		.irq = OMAP_GPIO_IRQ(OMAP5_TOUCH_IRQ_1),
 	},
 };
+#endif
 
 /*
  * I2C GPIO Expander - TCA6424
@@ -1194,6 +1195,7 @@ static struct omap2_hsmmc_info mmc[] = {
 	{}	/* Terminator */
 };
 
+#ifdef CONFIG_WL12XX_PLATFORM_DATA
 static struct regulator_consumer_supply omap5_sdp5430_vmmc3_supply[] = {
 	REGULATOR_SUPPLY("vmmc", "omap_hsmmc.2"),
 };
@@ -1263,6 +1265,7 @@ static void omap5_sdp5430_wifi_init(void)
 
 	platform_device_register(&omap_vwlan_device);
 }
+#endif
 
 /* USBB3 to SMSC LAN9730 */
 #define GPIO_ETH_NRESET_SEVM	172
