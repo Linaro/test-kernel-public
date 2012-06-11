10f87dcfade7abcc40cc8c557034bda8b19cee45
Vincent Stehl?? <v-stehle@ti.com>
2012-06-11 15:06:12 +0200

    Hack! Do not disable clock when wait_target_ready complains
    Signed-off-by: Vincent Stehlé <v-stehle@ti.com>

--- arch/arm/mach-omap2/omap_hwmod.c
+++ arch/arm/mach-omap2/omap_hwmod.c
@@ -1629,12 +1629,14 @@ static int _enable(struct omap_hwmod *oh)
 			_enable_sysc(oh);
 		}
 	} else {
-		_disable_clocks(oh);
+		printk("omap_hwmod: %s: Hack! NOT disabling clock.\n",
+			 oh->name);
+
 		pr_debug("omap_hwmod: %s: _wait_target_ready: %d\n",
 			 oh->name, r);
 
-		if (oh->clkdm)
-			clkdm_hwmod_disable(oh->clkdm, oh);
+		printk("omap_hwmod: %s: Hack! NOT disabling clkdm.\n",
+			 oh->name);
 	}
 
 	return r;
