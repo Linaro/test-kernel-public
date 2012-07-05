33f55204d3d55edcca735972b32e70a12da21cc6
Inderpal Singh <inderpal.singh@linaro.org>
2012-07-05 08:31:46 +0530

    backlight: Implement shutdown function in pwm_bl
    Implemented shutdown function so that LCD backlight can be
    turned off during system halt.
    
    Signed-off-by: Inderpal Singh <inderpal.singh@linaro.org>
    Signed-off-by: Tushar Behera <tushar.behera@linaro.org>

--- drivers/video/backlight/pwm_bl.c
+++ drivers/video/backlight/pwm_bl.c
@@ -277,6 +277,15 @@ static int pwm_backlight_remove(struct platform_device *pdev)
 	return 0;
 }
 
+static void pwm_backlight_shutdown(struct platform_device *pdev)
+{
+	struct backlight_device *bl = platform_get_drvdata(pdev);
+	struct pwm_bl_data *pb = dev_get_drvdata(&bl->dev);
+
+	pwm_config(pb->pwm, 0, pb->period);
+	pwm_disable(pb->pwm);
+}
+
 #ifdef CONFIG_PM
 static int pwm_backlight_suspend(struct device *dev)
 {
@@ -316,6 +325,7 @@ static struct platform_driver pwm_backlight_driver = {
 	},
 	.probe		= pwm_backlight_probe,
 	.remove		= pwm_backlight_remove,
+	.shutdown	= pwm_backlight_shutdown,
 };
 
 module_platform_driver(pwm_backlight_driver);
