ff55f63e5df792adf660f36be96b26a282b15dc9
Lorenzo Pieralisi <lorenzo.pieralisi@arm.com>
2012-07-13 15:30:06 +0100

    drivers: vexpress: add drivers infrastructure
    This patch provides the new kernel infrastructure needed by versatile
    express boards. Drivers are moved to drivers/misc/vexpress directory,
    where Kconfig and Makefile are created. A coalesced include file is
    created to cater for all versatile express required function declarations.

---
+++ include/linux/vexpress.h
@@ -0,0 +1,11 @@
+/*
+ * include/linux/vexpress.h
+ *
+ * Copyright (C) 2012 ARM Limited
+ *
+ * This file is subject to the terms and conditions of the GNU General Public
+ * License.  See the file COPYING in the main directory of this archive
+ * for more details.
+ *
+ * Versatile Express register definitions and exported functions
+ */
